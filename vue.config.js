9153e11e5d2d733b3a33fa8fed0bdb610d641eb4
novlan1 <1576271227@qq.com>
2026-06-17 17:37:36 +0800

    feat: add vconsole

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const {
+  GenVersionWebPlugin,
+  GenVersionMpPlugin,
+} = require('@plugin-light/webpack-plugin-gen-version');
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -8,10 +12,21 @@ function resolve(dir) {
 // 如果是 用户名.github.io/仓库名 则设置为 '/仓库名/'
 const GITHUB_PAGES_PATH = process.env.GITHUB_PAGES_PATH || '/tdesign-uniapp-starter-vue2-hx/';
 
+const plugins = []
+
+if (process.env.VUE_APP_PLATFORM !== 'h5') {
+  plugins.push(new GenVersionMpPlugin());
+} else {
+  plugins.push(new GenVersionWebPlugin());
+}
+
 module.exports = {
   // 生产环境下设置 publicPath，用于 GitHub Pages 部署
   publicPath: process.env.NODE_ENV === 'production' ? GITHUB_PAGES_PATH : '/',
   transpileDependencies: ['tdesign-uniapp', 'tdesign-uniapp-chat'],
+  configureWebpack: {
+    plugins,
+  },
   chainWebpack: (config) => {
     config.resolve.alias
       .set('@tdesign/uniapp', resolve('./uni_modules/tdesign-uniapp/components'))
